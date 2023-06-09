59ff705bff1eebd772194956061af1a6b54968d3
SANJUKTA1996 <sanjuktasahoo734@gmail.com>
2023-06-09 20:01:10 +0530

    first commit

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-// Basic server by using http,createServer.
-//const  http=require('http');
-// http.createServer((req,resp)=>{
-//     resp.write("<h1>Hello This is sanju</h1>");
-//     resp.end();
 
-// }).listen(3500);
-// const http=require('http');
-// function dataControl(req,resp){
-//     resp.write("<h1>Hello I am study Node js</h1>");
-//     resp.end();
-// }
-// http.createServer(dataControl).listen(3000);
-    
-//Arrow function wise wise
+  const fs=require('fs');
+  const path=require('path');
+ const dirpath=path.join(__dirname,'files');
+ //for(i=0;i<5;i++){
+ // fs.writeFileSync(dirpath+"/data.txt"+i+".txt","It is a simple text file");
+ // fs.writeFileSync(`data${i}.txt`,"It is a simple text file");
+ //}
+ fs.readdir(dirpath,(err,files)=>{
+  // console.warn(files);
+  files.forEach((item)=>{
+    console.log("file name is",item)
 
-const http=require('http');
-const dataControl = (req,resp)=>
-{
-    resp.write("<h1>Hello I am study Node js</h1>");
-    resp.end();
-}
-http.createServer(dataControl).listen(300);
-    
+
+  })
+
+ })
+
+
 
 
