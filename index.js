dcad0fe478f69dc0c3891420430b4790a7c01b22
SANJUKTA1996 <sanjuktasahoo734@gmail.com>
2023-06-09 12:59:58 +0530

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-const fs=require('fs');
-fs.writeFileSync("home.txt","This ia a home Page")
+// Basic server by using http,createServer.
+//const  http=require('http');
+// http.createServer((req,resp)=>{
+//     resp.write("<h1>Hello This is sanju</h1>");
+//     resp.end();
+
+// }).listen(3500);
+// const http=require('http');
+// function dataControl(req,resp){
+//     resp.write("<h1>Hello I am study Node js</h1>");
+//     resp.end();
+// }
+// http.createServer(dataControl).listen(3000);
+    
+//Arrow function wise wise
+
+const http=require('http');
+const dataControl = (req,resp)=>
+{
+    resp.write("<h1>Hello I am study Node js</h1>");
+    resp.end();
+}
+http.createServer(dataControl).listen(300);
+    
+
 
